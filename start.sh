90c59c1e82543f64a683addd77d470126f97bb77
think <tempor@ry.de>
2017-02-18 11:56:55 +0000

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker stop notebook; docker rm notebook
-docker run --name notebook -d -p 8888:8888 -v $(pwd):/notebook think/notebook
+docker run --name notebook \
+  -d -p 8888:8888 \
+  -v $(pwd):/notebook \
+  -v $(which docker):/usr/bin/docker \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  think/notebook
